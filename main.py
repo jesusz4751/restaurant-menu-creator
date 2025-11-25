93f7af11ed5485bcef2b5e28c5bcdd2a08bc7b3b
jesusz4751 <jesuseduardoz.4751@gmail.com>
2025-11-25 11:26:35 -0800

    Delete restaurant functionality, modify restaurant selection

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ valid_ans = ["r","c"]
 while acc_selection not in valid_ans:
   acc_selection = input("Please enter a valid response: ").lower()
 if acc_selection == "r":
-
   print(f"You currently have {len(user_res["restaurants"])} custom restaurants." )
   if len(user_res["restaurants"]) > 0:
     user_selection = input("Would you like to add (a), modify (m), or delete (d) an existing restaurant: ").lower()
@@ -25,14 +24,43 @@ if acc_selection == "r":
     restaurant["name"] = input("Enter a restaurant name: ")
   else:
     print("Here are all the currently created restaurants:")
-    for res in user_res["restaurants"]:
-      print(res)
+    for i, res in enumerate(user_res["restaurants"]):
+      print(f"{i+1}. {res["name"]}")
+    # User selects delete
     if user_selection == "d":
-      del_res = input("Please select a restaurant to delete: ")
-      while del_res not in user_res["restaurants"]:
-        del_res = input("Please select a valid restaurant: ")
+      try:
+        del_res = int(input("Please enter a restaurant number to delete: ")) - 1
+      except ValueError:
+        print("Invalid number")
+        del_res = -1
+      while True:
+        if del_res < 0 or del_res >= len(user_res["restaurants"]):
+          try:
+            del_res = int(input("Please select a valid restaurant: ")) - 1
+          except ValueError:
+            print("Invalid number")
+        else:
+          break
       user_res["restaurants"].pop(del_res)
-      # ADD CODE FOR SAVING TO JSON FILE
+      with open("data/user_restaurants.json", "w") as file:
+        json.dump(user_res,file,indent=2)
       sys.exit("Thank you for using the progam, we hope to see you again soon!")
+    # User selects modify
+    else:
+      try:
+        mod_res = int(input("Please enter a restaurant number to modify: ")) - 1
+      except ValueError:
+        print("Invalid number")
+        mod_res = -1
+      while True:
+        if mod_res < 0 or mod_res >= len(user_res["restaurants"]):
+          try:
+            mod_res = int(input("Please select a valid restaurant: ")) - 1
+          except ValueError:
+            print("Invalid number")
+        else:
+          break
+      restaurant = user_res["restaurants"][mod_res]
+      print(restaurant["name"])
 else:
   print("customer")
